4ddcb51e7a9a675904d220a9924fe3ac53ed84cc
Tomeu <tomeu.mayol@etechyachting.com>
2023-08-17 14:58:57 +0200

    FIX Theme change type

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,29 +12,118 @@ import foto1 from '../img/img1.jpg'
     setCurrentStyle(newStyle); 
   };
 
-  return (
-    <div id='containerHome'>
-      <h1 id='containerHomeTitle'>Welcome to{currentStyle == 'body-style-2' ? (<span id='homeTextGradient'> Origin </span>) : (<span id='homeTextGradient2'> Origin </span>) }</h1>
-      <p id='principalParagraph'>A marketing website theme built with <a id='reactLink' href="https://react.dev/" target="_blank">React</a> and carefully crafted for startups and businesses.</p>
+
+    return (
+        
+      <div>
+      {(() => {
+        if (currentStyle === 'body-style-1') {
+          return (
+            <div id='containerHome'>
+            <h1 id='containerHomeTitle'>Welcome to <span id='homeText'> Origin </span></h1>
+            <p id='principalParagraph'>A marketing website theme built with <a id='reactLink' href="https://react.dev/" target="_blank">React</a> and carefully crafted for startups and businesses.</p>
+            
+            <div id='homeContainerButtons'>
+              <button id='getStartedButton'>Get Started</button>
+              <button id='styleGuideButton'>Style Guide </button>
+            </div>
+    
+            <div className='buttonThemes'>
+              <button id='buttonClassic' onClick={() => handleStyleChange('body-style-1')}>Classic</button>
+              <button id='buttonDark' onClick={() => handleStyleChange('body-style-2')}>Dark</button>
+              <button id='buttonEarth' onClick={() => handleStyleChange('body-style-3')}>Earth</button>
+              <button id='buttonOcean' onClick={() => handleStyleChange('body-style-4')}>Ocean</button>
+            </div>
+
+            <div id='foto1Container'>
+            <img id='foto1' src={foto1}/>
+            </div>
+            
+          </div>
+          )
+        } else if (currentStyle === 'body-style-2') {
+          return (
+            <div id='containerHome'>
+            <h1 id='containerHomeTitle'>Welcome to <span id='homeText2'> Origin </span></h1>
+            <p id='principalParagraph'>A marketing website theme built with <a id='reactLink' href="https://react.dev/" target="_blank">React</a> and carefully crafted for startups and businesses.</p>
+            
+            <div id='homeContainerButtons'>
+              <button id='getStartedButton'>Get Started</button>
+              <button id='styleGuideButton'>Style Guide </button>
+            </div>
+
+            <div className='buttonThemes'>
+              <button id='buttonClassic' onClick={() => handleStyleChange('body-style-1')}>Classic</button>
+              <button id='buttonDark' onClick={() => handleStyleChange('body-style-2')}>Dark</button>
+              <button id='buttonEarth' onClick={() => handleStyleChange('body-style-3')}>Earth</button>
+              <button id='buttonOcean' onClick={() => handleStyleChange('body-style-4')}>Ocean</button>
+            </div>
+
+            <div id='foto1Container'>
+            <img id='foto1' src={foto1}/>
+            </div>
+            
+          </div>
+          )
+        } else if (currentStyle === 'body-style-3') {
+          return (
+            <div id='containerHome'>
+            <h1 id='containerHomeTitle'>Welcome to <span id='homeText3'> Origin </span></h1>
+            <p id='principalParagraph'>A marketing website theme built with <a id='reactLink' href="https://react.dev/" target="_blank">React</a> and carefully crafted for startups and businesses.</p>
+            
+            <div id='homeContainerButtons'>
+              <button id='getStartedButton'>Get Started</button>
+              <button id='styleGuideButton'>Style Guide </button>
+            </div>
+            <div className='buttonThemes'>
+              <button id='buttonClassic' onClick={() => handleStyleChange('body-style-1')}>Classic</button>
+              <button id='buttonDark' onClick={() => handleStyleChange('body-style-2')}>Dark</button>
+              <button id='buttonEarth' onClick={() => handleStyleChange('body-style-3')}>Earth</button>
+              <button id='buttonOcean' onClick={() => handleStyleChange('body-style-4')}>Ocean</button>
+            </div>
+    
+            <div id='foto1Container'>
+            <img id='foto1' src={foto1}/>
+            </div>
+            
+          </div>
+          )
+        } else if (currentStyle === 'body-style-4') {
+          return (
+            <div id='containerHome'>
+            <h1 id='containerHomeTitle'>Welcome to <span id='homeText4'> Origin </span></h1>
+            <p id='principalParagraph'>A marketing website theme built with <a id='reactLink' href="https://react.dev/" target="_blank">React</a> and carefully crafted for startups and businesses.</p>
+            
+            <div id='homeContainerButtons'>
+              <button id='getStartedButton'>Get Started</button>
+              <button id='styleGuideButton'>Style Guide </button>
+            </div>
+
+            <div className='buttonThemes'>
+              <button id='buttonClassic' onClick={() => handleStyleChange('body-style-1')}>Classic</button>
+              <button id='buttonDark' onClick={() => handleStyleChange('body-style-2')}>Dark</button>
+              <button id='buttonEarth' onClick={() => handleStyleChange('body-style-3')}>Earth</button>
+              <button id='buttonOcean' onClick={() => handleStyleChange('body-style-4')}>Ocean</button>
+            </div>
+
+            <div id='foto1Container'>
+            <img id='foto1' src={foto1}/>
+            </div>
+            
+          </div>
+          )
+        } 
+      })()}
+            
+            
+
+
       
-      <div id='homeContainerButtons'>
-        <button id='getStartedButton'>Get Started</button>
-        <button id='styleGuideButton'>Style Guide </button>
+            
       </div>
 
-      <div className='buttonThemes'>
-        <button id='buttonClassic' onClick={() => handleStyleChange('body-style-1')}>Classic</button>
-        <button id='buttonDark' onClick={() => handleStyleChange('body-style-2')}>Dark</button>
-        <button id='buttonEarth' onClick={() => handleStyleChange('body-style-3')}>Earth</button>
-        <button id='buttonOcean' onClick={() => handleStyleChange('body-style-4')}>Ocean</button>
-      </div>
-
-      
-      <div id='foto1Container'>
-      <img id='foto1' src={foto1}/>
-      </div>
-      
-    </div>
-  )
+    )
+  
+ 
 }
 export default Home;
