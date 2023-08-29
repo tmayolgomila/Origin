399acb2f247370c9bb23ae8af5faf5a7af72f0aa
Tomeu <tomeu.mayol@etechyachting.com>
2023-08-29 15:11:34 +0200

    ADD classvalues

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Layout from './Layout';
 
 const Blogs = () => {
 
-  const [currentStyle, setCurrentStyle] = useState('body-style-2');
-  console.log(document.body.classList.value)
   return (
     <div>
       {(() => {
-        if (currentStyle === 'body-style-1') {
+        if (document.body.classList.value === 'body-style-1') {
           return <div>style1</div>;
-        } else if (currentStyle === 'body-style-2') {
+        } else if (document.body.classList.value === 'body-style-2') {
           return <div>style2</div>;
-        } else if (currentStyle === 'body-style-3') {
+        } else if (document.body.classList.value === 'body-style-3') {
           return <div>style3</div>;
-        } else if (currentStyle === 'body-style-4') {
+        } else if (document.body.classList.value === 'body-style-4') {
           return <div>style4</div>;
         }
       })()}
